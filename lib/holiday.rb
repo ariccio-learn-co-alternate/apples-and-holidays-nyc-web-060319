f2ff5ac56b2e08466e0d8ef9e5bfefc888937bc4
Alexander Riccio <test35965@gmail.com>
2019-05-23 01:01:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_str = "#{season}:"
     puts season_str.capitalize!
     holiday_hash[season].keys.each do |each_holiday|
-      each_holiday_str = each_holiday
+      each_holiday_str = "#{each_holiday}"
       each_holiday_str = each_holiday_str.capitalize!
       holiday_str = " #{each_holiday_str}:"
       holiday_hash[season][each_holiday].each do |supply|
