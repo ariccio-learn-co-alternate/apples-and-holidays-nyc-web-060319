8a867a1a76bc2c2c2e298594217b312c6e97545e
Alexander Riccio <test35965@gmail.com>
2019-05-23 01:02:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash[season].keys.each do |each_holiday|
       each_holiday_str = "#{each_holiday}"
       each_holiday_str = each_holiday_str.capitalize!
+      each_holiday_str.replace("_")
       holiday_str = "  #{each_holiday_str}:"
       holiday_hash[season][each_holiday].each do |supply|
         holiday_str += " "
